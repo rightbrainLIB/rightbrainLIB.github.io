4b8829654971edabdb4577f0bbc3b4d86fde2235
thl <thl@rightbrain.co.kr>
2023-10-17 16:24:27 +0900

    계좌번호 입력창 스í타일 완료

--- src/pages/TransitView.jsx
+++ src/pages/TransitView.jsx
@@ -4,32 +4,14 @@ import {Row, Col, Input} from "antd";
 import CameraTransit from "../components/Buttons/CameraTransit.jsx";
 import PNTransit from "../components/Buttons/PNTransit.jsx";
 import UserTransitInput from "../components/transit/UserTransitInput.jsx";
-import {useEffect, useRef} from "react";
 
 const TransitView = () => {
-  const accountNumRef = useRef(null);
-
-  useEffect(() => {
-    if (accountNumRef.current) {
-      console.log('accountNumRef = ' ,accountNumRef.current)
-    }
-  }, [accountNumRef.current]);
-
   return (
     <>
       <KBContainer>
         <KBHeader />
         <UserTransitInput>
-          <h2>누구에게 보낼까요?</h2>
-          <Input ref={accountNumRef} placeholder={"계좌번호 입력"} inputMode="none" />
-          <div>
-            <CameraTransit />
-            <p>촬영이체</p>
-          </div>
-          <div>
-            <PNTransit />
-            <p>연락처이체</p>
-          </div>
+
         </UserTransitInput>
       </KBContainer>
     </>
